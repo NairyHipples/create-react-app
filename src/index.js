1216db93274656b39fcfcf5a564cb41098cb634f
Jonatan Östling <jonatan.ostling@avensia.com>
2019-04-16 08:19:20 +0200

    Playing around with the children property in React

--- src/index.js
+++ src/index.js
@@ -2,20 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import Comment from "./Comment";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    const authorName = "Alex";
-    const comment = "What a nice website!";
-    const time = new Date().toLocaleTimeString();
+    const authorNames = [
+        {
+            name: "Alex",
+            comment: "Wow this is a great website!",
+            time: "13:34"
+        },
+        {
+            name: "Kim",
+            comment: "I agree with Alex on this one!",
+            time: "13:40"
+        },
+        { name: "Max", comment: "Cool :)", time: "14:00" },
+        {
+            name: "Alex",
+            comment: "Hey Alex, I like your name! :) Also, cool website dude!",
+            time: "14:14"
+        }
+    ];
 
     return (
         <div className="ui container comments">
-            <Comment
-                image={faker.image.avatar()}
-                authorName={authorName}
-                time={time}
-                comment={comment}
-            />
+            {authorNames.map((author, i) => {
+                return (
+                    <ApprovalCard key={i}>
+                        <Comment
+                            image={faker.image.avatar()}
+                            authorName={author.name}
+                            time={author.time}
+                            comment={author.comment}
+                        />
+                    </ApprovalCard>
+                );
+            })}
         </div>
     );
 };
