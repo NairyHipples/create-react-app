319c141c0f8fa00ce305907ecbede32933e4be1c
Jonatan Östling <jonatan.ostling@avensia.com>
2019-04-16 11:06:03 +0200

    Starting to play with geolocation

---
+++ src/Components/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>Season Display says: Helllllo!</div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,44 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
-import Comment from "./Comment";
-import ApprovalCard from "./ApprovalCard";
+import SeasonDisplay from "./Components/SeasonDisplay";
+// import faker from "faker";
+// import Comment from "./Components/Comment";
+// import ApprovalCard from "./Components/ApprovalCard";
 
 const App = () => {
-    const authorNames = [
-        {
-            name: "Alex",
-            comment: "Wow this is a great website!",
-            time: "13:34"
-        },
-        {
-            name: "Kim",
-            comment: "I agree with Alex on this one!",
-            time: "13:40"
-        },
-        { name: "Max", comment: "Cool :)", time: "14:00" },
-        {
-            name: "Alex",
-            comment: "Hey Alex, I like your name! :) Also, cool website dude!",
-            time: "14:14"
-        }
-    ];
+    // const authorNames = [
+    //     {
+    //         name: "Alex",
+    //         comment: "Wow this is a great website!",
+    //         time: "13:34"
+    //     },
+    //     {
+    //         name: "Kim",
+    //         comment: "I agree with Alex on this one!",
+    //         time: "13:40"
+    //     },
+    //     { name: "Max", comment: "Cool :)", time: "14:00" },
+    //     {
+    //         name: "Alex",
+    //         comment: "Hey Alex, I like your name! :) Also, cool website dude!",
+    //         time: "14:14"
+    //     }
+    // ];
 
+    window.navigator.geolocation.getCurrentPosition(
+        position => {
+			if (position.coords.latitude > 0) {
+				console.log('Northern hemisphere');
+			} else {
+				console.log('Southern hemisphere');
+			}
+		},
+        err => {
+			console.log(err.message);
+		}
+    );
     return (
-        <div className="ui container comments">
-            {authorNames.map((author, i) => {
-                return (
-                    <ApprovalCard key={i}>
-                        <Comment
-                            image={faker.image.avatar()}
-                            authorName={author.name}
-                            time={author.time}
-                            comment={author.comment}
-                        />
-                    </ApprovalCard>
-                );
-            })}
-        </div>
+        <SeasonDisplay />
+        // <div className="ui container comments">
+        //     {authorNames.map((author, i) => {
+        //         return (
+        //             <ApprovalCard key={i}>
+        //                 <Comment
+        //                     image={faker.image.avatar()}
+        //                     authorName={author.name}
+        //                     time={author.time}
+        //                     comment={author.comment}
+        //                 />
+        //             </ApprovalCard>
+        //         );
+        //     })}
+        // </div>
     );
 };
 
